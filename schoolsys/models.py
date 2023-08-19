78f08bf02489c8d627d7d0fdc459863c4845e6ae
uxama-jamil <usama_jamil@hotmail.com>
2023-08-19 18:28:03 +0500

    creating models

--- schoolsys/models.py
+++ schoolsys/models.py
@@ -1,5 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class teacher(models.Model):
+    name = models.CharField(max_length=20)
+    subject = models.CharField(max_length=20)
+    age = models.IntegerField(null=True,blank=True)
 
+class student(models.Model):
+    name = models.CharField(max_length=20)
+    subject = models.CharField(max_length=20)
+    class_name = models.CharField(max_length=10)
+    age = models.IntegerField(null=True,blank=True)
+    teacher_id = models.ForeignKey(teacher,on_delete=models.CASCADE) #foriegn key 
 
