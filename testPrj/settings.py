ea2445d35f662217c69c0dd5def5860bf1c7f792
uxama-jamil <usama_jamil@hotmail.com>
2023-08-19 18:17:29 +0500

    databse connection my sql

--- testPrj/settings.py
+++ testPrj/settings.py
@@ -74,9 +74,19 @@ WSGI_APPLICATION = 'testPrj.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+
+     # database configs
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django',
+        'USER': 'root',
+        'PASSWORD': '07b070ee',
+        'HOST': 'localhost',  # Use '127.0.0.1' if 'localhost' doesn't work
+        'PORT': '3306',       # Default MySQL port
     }
 
     
