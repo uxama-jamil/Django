5711be177f2146959be4f098b1bf77384a02e2ba
uxama-jamil <usama_jamil@hotmail.com>
2023-08-20 16:30:54 +0500

    serializer implemenation and patch request creation

--- schoolsys/views.py
+++ schoolsys/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import viewsets,views
 from schoolsys.models import student,teacher
 from rest_framework.response import Response
+from .serializer import TeacherSerializer
 
 # Create your views here.
 
@@ -11,18 +12,40 @@ class FetchTeacherView(views.APIView):
     def get(self,request):
         print("request is ",request.GET.get('teacher_id'))
         tid = request.GET.get('teacher_id')#getting query params
-        query=teacher.objects.all().values() #getting whole record
+        query=teacher.objects.all() #getting whole record
+        ser = TeacherSerializer(query,many=True) # many true means it will have more than one record
         
         if tid:
-            query=teacher.objects.filter(id=tid).values() #getting specifc record
-            return Response(query) #returning response
-        return Response(query)
+            query=teacher.objects.get(id=tid) #getting specifc record
+            ser = TeacherSerializer(query)  #passing queryset for json coversion
+
+            return Response(ser.data) #returning response
+        return Response(ser.data)
     
     def post(self,request):
         data=request.data # request body
-        teacher.objects.create(name=data['name'],subject=data['subject'],age=data['age']) #creating record
+        # teacher.objects.create(name=data['name'],subject=data['subject'],age=data['age']) #creating record
+        ser = TeacherSerializer(data=data) #using serializer passing request body in data argument which created record 
+        if ser.is_valid(): #checking if record is valid
+            ser.save() # record creation
+            return Response({'status':True,'message':ser.data},200)
+            
+        else:
+            return Response({'status':False,'error':ser.errors},400)
         
-        return Response("data add sucessfully")
+    def patch(self,request):
+        data=request.data # request body
+        tid = request.GET.get('teacher_id')#getting query params , this way not safe
+
+        query=teacher.objects.get(id=data['tid']) #getting specifc record 
+        
+        ser = TeacherSerializer(query,data=data,partial=True) # partial true mean it will update specific fields if there is a queryset
+        if ser.is_valid():
+            ser.save()
+            return Response({'status':True,'message':ser.data},200)
+            
+        else:
+            return Response({'status':False,'error':ser.errors},400)
 
 
 #student    
