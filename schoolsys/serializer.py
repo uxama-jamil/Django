19484d7f71e71b4efed187bb18c756b1a0bedfbc
uxama-jamil <usama_jamil@hotmail.com>
2023-08-27 18:09:11 +0500

    custom field single and multi validation

--- schoolsys/serializer.py
+++ schoolsys/serializer.py
@@ -11,3 +11,15 @@ class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = student
         fields = "__all__"
+
+    def validate_age(self,val): #single field validation 
+        print("value is ", val)
+        if val>100:
+            raise serializers.ValidationError({"age":"age cannot be greater than 100"})
+        return val
+    
+    def validate(self,val): # multi field validation
+        if val['subject'].lower() in ['urdu','sindhi'] and len(val['name'])>10:
+            raise serializers.ValidationError({'subject':f"subject cannot be {val['subject']}",
+                                               'name':'name cannot be greater than 10'})
+        return val
